b0367dbeaf8f9533a28f5fee2610b75bb3d8554e
hanotao <18073811001@163.com>
2020-03-30 18:27:47 +0800

    项目第一天

---
+++ 22_React_Project/react_app/src/redux/reducers.js
@@ -0,0 +1,18 @@
+import {combineReducers} from "redux" //合并
+
+
+function xxx(state=0,action){
+  return state
+}
+
+function yyy(state=0,action){
+  return state
+}
+
+
+
+
+export default combineReducers({
+  xxx,
+  yyy
+})
